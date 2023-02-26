35e498e049e8c9993726f32fb5b9fb4f569e0ec3
Tenebris <naniiloveminecraft@outlook.com>
2023-02-26 11:42:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 import json
+from discord.ext import commands
+from dislash import InteractionClient
+from discord import app_commands
+from discord import __author__
+import asyncio
+from discord.ext import commands
 
 with open('C:/Users/modib/Documents/kali/py/MusicBot/config.json') as f:
    data = json.load(f)
@@ -14,7 +20,8 @@ TOKEN = data["TOKEN"]
 intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
-ctx = "*"
+PREFIX = "*"
+tree = app_commands.CommandTree(client)
 #endregion
 
 @client.event
@@ -22,25 +29,33 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="my Master orders :)"))
 
-async def Play(message):
-    channel = ctx.author.voice.channel
-    if channel == None :
-        await message.channel.Send("You aren't in a voice channel yet!")
-    else :
-        vc = await channel.connect()
-        await message.channel.send("Joined Successfully!")
-
-async def Stop(message):
-    channel = ctx.author.voice.channel
-    await message.voice_client.disconnect()
+@client.event
+async def Leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+    print("Left Voice Channel")
 
 @client.event
 async def on_message(message:discord.Message):
-    if message.author.bot or not(str(message.content).startswith(ctx)):
+    if message.author.bot or not(str(message.content).startswith(PREFIX)):
         return
-    if message.channel.id == (ctx, "Start") :
-        Play
-    if message.channel.id == (ctx, "Stop") :
-        Stop
+    args = message.content.split(" ")
+    args[0] = args[0][1::]
+
+    if args[0] == "Start":
+        await message.channel.send("Comingg !!!")
+    channel = message.author.voice.channel
+    vc = channel.connect()
+    print("Joined Voice Channel")
+    await message.channel.send("Joined Successfully!")
+    if channel is None :
+        await message.channel.send("You aren't in a voice channel yet!")
+
+    elif args[0] == "Stop" :
+        server = message.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+    print("Left Voice Channel")
 
 client.run(TOKEN)
